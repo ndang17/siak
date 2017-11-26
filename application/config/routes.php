b45ad218ed6f245e68bc18b4d400940d47648c83
unknown <nndg.ace3@gmail.com>
2017-11-26 18:23:51 +0700

    ok

---
+++ application/config/routes.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$route['default_controller'] = 'c_dashboard';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
+
+
+$route['dashboard'] = 'c_dashboard';
+$route['profile'] = 'c_dashboard/profile';
