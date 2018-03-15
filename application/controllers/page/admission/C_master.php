b7d6a7d6ef82957181ab916af23a2c85dbfdc4b7
alhadirahman22podomoro <alhadi.rahman@podomorouniversity.ac.id>
2018-03-15 17:21:58 +0700

    Update Master Admision 1.0

--- application/controllers/page/admission/C_master.php
+++ application/controllers/page/admission/C_master.php
@@ -3,10 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class C_master extends MY_Controller {
 
+    private $data = array();
+
     function __construct()
     {
         parent::__construct();
         $this->load->model('m_sendemail');
+        $this->load->model('master/m_master');
+        $this->data['department'] = parent::__getDepartement();
     }
 
     public function temp($content)
@@ -24,15 +28,13 @@ class C_master extends MY_Controller {
 
     public function sma()
     {
-        $data['department'] = parent::__getDepartement();
-        $content = $this->load->view('page/'.$data['department'].'/master/sma',$data,true);
+        $content = $this->load->view('page/'.$this->data['department'].'/master/sma',$this->data,true);
         $this->temp($content);
     }
 
     public function sma_integration()
     {
-        $data['department'] = parent::__getDepartement();
-        $content = $this->load->view('page/'.$data['department'].'/master/sma_integration',$data,true);
+        $content = $this->load->view('page/'.$this->data['department'].'/master/sma_integration',$this->data,true);
         $this->temp($content);
 
     }
@@ -40,17 +42,15 @@ class C_master extends MY_Controller {
     public function sma_table()
     {
         $token = $this->input->post('token');
-        $data['department'] = parent::__getDepartement();
-        $data['token'] = $token;
-        $this->load->view('page/'.$data['department'].'/master/sma_table',$data);
+        $this->data['token'] = $token;
+        $this->load->view('page/'.$this->data['department'].'/master/sma_table',$this->data);
     }
 
     public function config_set_email()
     {
-            $data['department'] = parent::__getDepartement();
             $getEmailConfig = $this->m_sendemail->loadEmailConfig();
-            $data['email'] = $getEmailConfig['setting'];
-            $content = $this->load->view('page/'.$data['department'].'/master/set_email',$data,true);
+            $this->data['email'] = $getEmailConfig;
+            $content = $this->load->view('page/'.$this->data['department'].'/master/set_email',$this->data,true);
             $this->temp($content);
     }
 
@@ -74,8 +74,59 @@ class C_master extends MY_Controller {
         $pwd = $input['pwd'];
         $smtp_port = $input['smtp_port'];
         $smtp_host = $input['smtp_host'];
-        $save_email = $this->m_sendemail->save_email($smtp_host,$smtp_port,$email,$pwd);
+        $text = $input['text'];
+        $save_email = $this->m_sendemail->save_email($smtp_host,$smtp_port,$email,$pwd,$text);
+        return print_r(json_encode($save_email));
+    }
+
+    public function total_account()
+    {
+        $content = $this->load->view('page/'.$this->data['department'].'/master/total_account',$this->data,true);
+        $this->temp($content);
+    }
+
+    public function load_table_total_account()
+    {
+        $this->data['getColoumn'] = $this->m_master->getColumnTable('db_admission.count_account');
+        $this->data['getData'] = $this->m_master->showData('db_admission.count_account');
+        echo $this->load->view('page/'.$this->data['department'].'/master/table_total_account',$this->data,true);
         
+    }
+
+    public function modalform($table)
+    {
+        $input = $this->getInputToken();
+        $this->data['action'] = $input['Action'];
+        $this->data['id'] = $input['CDID'];
+        $this->data['getColoumn'] = $this->m_master->getColumnTable('db_admission.'.$table);
+        $this->data['getData'] = null;
+        if ($this->data['id'] != '') {
+            $this->data['getData'] = $this->m_master->caribasedprimary('db_admission.'.$table,'ID',$this->data['id']);
+        }
+        echo $this->load->view('page/'.$this->data['department'].'/master/modalform',$this->data,true);
+    }
+
+    public function submit_count_account()
+    {
+        $input = $this->getInputToken();
+
+        switch ($input['Action']) {
+            case 'add':
+                $this->m_master->inserData_count_account($input['CountAccount']);
+                break;
+            case 'edit':
+                $this->m_master->editData_count_account($input['CountAccount'],$input['CDID']);
+                break;
+            case 'delete':
+                $this->m_master->delete_count_account($input['CDID']);
+                break;        
+            case 'getactive':
+                $this->m_master->getActive_count_account($input['CDID'],$input['Active']);
+                break;    
+            default:
+                # code...
+                break;
+        }
     }
 
 }
--- application/models/master/M_master.php
+++ application/models/master/M_master.php
@@ -10,5 +10,83 @@ class M_master extends CI_Model {
     return $data->result_array();
   }
 
+  public function showData($tabel)
+  {
+  	$sql = "select * from ".$tabel; 
+  	$query=$this->db->query($sql, array());
+  	return $query->result();
+  }
+
+  public function showDataActive($tabel)
+  {
+  	$sql = "select * from ".$tabel." where active = 1"; 
+  	$query=$this->db->query($sql, array());
+  	return $query->result();
+  }
+
+  public function caribasedprimary($tabel,$fieldPrimary,$valuePrimary)
+  {
+  	$sql = "select * from ".$tabel." where ".$fieldPrimary." = ?"; 
+  	$query=$this->db->query($sql, array($valuePrimary));
+  	return $query->result_array();
+  }
+
+  public function getColumnTable($table)
+  {
+  	$arr = array();
+  	$sql = "SHOW COLUMNS FROM ".$table; 
+  	$query=$this->db->query($sql, array())->result();
+  	$temp = array();
+  	foreach ($query as $key) {
+  		$temp[] = $key->Field;
+  	}
+  	$arr = array('query' => $query,'field' => $temp); 
+  	return $arr;
+  }
+
+  public function inserData_count_account($CountAccount)
+  {
+  	$dataSave = array(
+  	        'CountAccount' => $CountAccount,
+  	        'CreateAT' => date('Y-m-d'),
+  	);
+  	$this->db->insert('db_admission.count_account', $dataSave);
+
+  	$sql = "select a.ID from db_admission.count_account as a where a.active = 1 order by a.ID desc limit 1";
+  	$query=$this->db->query($sql, array())->result_array();
+  	$ID = $query[0]['ID'];
+
+  	$sql = "update db_admission.count_account set Active = 0 where ID != ".$ID;
+  	$query=$this->db->query($sql, array());
+
+  }
+
+  public function editData_count_account($CountAccount,$ID)
+  {
+  	$sql = "update db_admission.count_account set CountAccount = ? where ID = ".$ID;
+  	$query=$this->db->query($sql, array($CountAccount));
+  }
+
+  public function getActive_count_account($ID,$Active)
+  {
+  	if ($Active == 0) {
+  		$sql = "update db_admission.count_account set Active = 1 where ID = ".$ID;
+  		$sql2 = "update db_admission.count_account set Active = 0 where ID != ".$ID;
+  		$query2=$this->db->query($sql2, array());
+  	}
+  	else
+  	{
+  		$sql = "update db_admission.count_account set Active = 0 where ID = ".$ID;
+  	}
+  	$query=$this->db->query($sql, array($CountAccount));
+
+  }
+
+  public function delete_count_account($ID)
+  {
+  	$sql = "delete from db_admission.count_account where ID = ".$ID;
+  	$query=$this->db->query($sql, array($CountAccount));
+  }
+
 
 }
